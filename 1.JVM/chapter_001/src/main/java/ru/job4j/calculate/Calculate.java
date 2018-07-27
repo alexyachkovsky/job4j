1c0eb8c08066a511d829087309fbae3c4437e96c
Alex Yachkovsky <mailto:yachkovskiy@yahoo.com>
2018-07-27 14:54:21 +0400

    1.3. JavaDoc. Code convention [#4087]

--- 1.JVM/chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ 1.JVM/chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+ *Class Calculate решения задачи части 001 урок 1
+ *@author Alexey Yachkovsky
+ *@since 27.07.2018
+*/
 public class Calculate {
-	
+	/**
+     * Метод main 
+     *@param args - args. выводит строчку приветствия
+    */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// Ну вот, пришло время и мне поприветствовать этот мир.
---
+++ 1.JVM/chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *Package for calculate task.
+ *
+ *Class Calculate решения задачи части 001 урок 1
+ *@author Alexey Yachkovsky (mailto:yachkovsiy@yahoo.com)
+ *@version $Id$
+ *@since 0.1
+*/
+package ru.job4j.calculate;
