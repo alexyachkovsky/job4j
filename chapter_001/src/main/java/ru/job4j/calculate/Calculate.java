445b650adb59f2359528f8e625ee51aa1b7c8dd8
Alex Yachkovsky <mailto:yachkovskiy@yahoo.com>
2018-09-11 10:59:15 +0300

    2.2. Импорт Maven проекта. [#31168]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,22 +4,22 @@ package ru.job4j.calculate;
  *@author Alexey Yachkovsky (mailto:yachkovsiy@yahoo.com)
  *version $Id$
  *@since 27.07.2018
-*/
+ */
 public class Calculate {
-/**
- *Method main 
- *@param args - args. выводит строчку приветствия
-*/
+	/**
+	 *Method main
+	 *@param args - args. выводит строчку приветствия
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// Ну вот, пришло время и мне поприветствовать этот мир.
 	}
-/**
- *Method echo. 
- *@param name Your name.
- *@return Echo plus your name.
- */
- public String echo(String name) {
-	return "Echo, echo, echo : " + name;
- }
+	/**
+	 *Method echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
